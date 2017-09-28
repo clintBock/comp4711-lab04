9aa3e80346accbd595544d1aa21eab1ad057e5ff
Clint <cougarclint@hotmail.com>
2017-09-28 14:05:23 -0700

    added the delta controller and changed alpha to able

--- application/config/config.php
+++ application/config/config.php
@@ -4,10 +4,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 // the menu basics (text navbar)
 $config['menu_choices'] = array(
     'menudata' => array(
-        array('name' => 'Alpha', 'link' => '/Alpha'),
+        array('name' => 'Alpha', 'link' => '/Able'),
         array('name' => 'Bravo', 'link' => '#'),
         array('name' => 'Charlie', 'link' => '#'),
-        array('name' => 'Delta', 'link' => '#'),
+        array('name' => 'Delta', 'link' => '/Delta/Force'),
         array('name' => 'Echo', 'link' => '#'),
         array('name' => 'Foxtrot', 'link' => '#'),
         array('name' => 'Golf', 'link' => '#'),
