8f0a9a75a725fa9627fb454cccc51660bffa2374
Brayden <btraas@gmail.com>
2017-09-28 14:57:22 -0700

    closes #9 - India (a utility controller)

--- application/config/config.php
+++ application/config/config.php
@@ -12,7 +12,7 @@ $config['menu_choices'] = array(
         array('name' => 'Foxtrot', 'link' => '#'),
         array('name' => 'Golf', 'link' => '#'),
         array('name' => 'Hotel', 'link' => '#'),
-        array('name' => 'India', 'link' => '#'),
+        array('name' => 'India', 'link' => '/India'),
         array('name' => 'Juliet', 'link' => '#'),
         array('name' => 'Kilo', 'link' => '#'),
         )
